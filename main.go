8c7e3e162cd961e7b96aa2ac65ad92d84dc02d9a
Devi <167757357+visakadev@users.noreply.github.com>
2025-09-04 05:14:05 +0200

    Implement main function with application initialization and HTTP server setup

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"net/http"
+	"time"
+
+	"github.com/visakadev/go/internal/app"
+)
+
 func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("We are running out app")
+
+	server := &http.Server{
+		Addr:        ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
 
 }
