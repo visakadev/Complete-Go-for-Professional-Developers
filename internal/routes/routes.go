3849fd1cd4557efa7f0c842cd5acedcdc2694d78
Devi <167757357+visakadev@users.noreply.github.com>
2025-09-10 16:30:09 +0200

    Add .gitignore and docker-compose.yml; update routes for workout handling

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,6 +8,10 @@ import (
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/health", app.HealthCheck)
+	// Workout
+	r.Get("/workout/{id}", app.WorkoutHandler.HandleWorkoutByID)
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+
 	return r
 
 }
