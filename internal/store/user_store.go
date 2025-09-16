b2c9adde5e395d1138183076885df37cd5927383
Devi <167757357+visakadev@users.noreply.github.com>
2025-09-16 20:14:27 +0200

    feat: add user store implementation with Postgres support

---
+++ internal/store/user_store.go
@@ -0,0 +1,38 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+	// "golang.org/x/crypto/bcrypt"
+)
+
+type password struct {
+	plainText *string
+	hash      []byte
+}
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash password  `json:"-"`
+	Bio          string    `json:"bio"`
+	CreatedAt    time.Time `json:"create_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type PostgresUserStore struct {
+	db *sql.DB
+}
+
+func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
+	return &PostgresUserStore{
+		db: db,
+	}
+}
+
+type UserStore interface {
+	CreateUser(*User) error
+	GetUserByUsername(username string) (*User, error)
+	UpdateUser(*User) error
+}
