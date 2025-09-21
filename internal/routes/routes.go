6148de7d9e14cabf2e13b64205542dada5855e87
Devi <167757357+visakadev@users.noreply.github.com>
2025-09-21 14:47:48 +0200

    refactor: group workout routes under authentication middleware

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,17 +7,19 @@ import (
 
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
-	r.Get("/health", app.HealthCheck)
-	// Workout
-	r.Get("/workout/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
-	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
-	r.Put("/workout/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
-	r.Delete("/workout/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
+	r.Group(func(r chi.Router) {
+		r.Use(app.Middleware.Authenticate)
+		// Workout
+		r.Get("/workout/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+		r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+		r.Put("/workout/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
+		r.Delete("/workout/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
+	})
 
+	r.Get("/health", app.HealthCheck)
 	// Users
 	r.Post("/users", app.UserHandler.HandleRegisterUser)
-
-	//Token
+	// Token
 	r.Post("/tokens/authentication", app.TokenHandler.HandleCreateToken)
 
 	return r
